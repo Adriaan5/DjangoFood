03020390cf3a019527d4b28b3e1308d473fba658
Adriaan5 <adrianismaelpereyrasanchez@gmail.com>
2024-11-25 10:41:02 -0500

    Update settings.py

--- SistemaReservas/SistemaReservas/settings.py
+++ SistemaReservas/SistemaReservas/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'SistemaReservas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestion',
-        'USER': 'postgres',
-        'PASSWORD': 'psadrian1027',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  
     }
 }
 
